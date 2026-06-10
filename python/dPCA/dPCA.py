Fix the marginal_variances function to coincide with the formula in the paper.

python/dPCA/dPCA.py
@@ -925,14 +925,14 @@ class dPCA(BaseEstimator):
 
         """
         X = self._zero_mean(X)
-        total_variance = np.sum((X - np.mean(X))**2)
+        total_variance = np.sum(X**2)
 
         def marginal_variances(marginal):
             ''' Computes the relative variance explained of each component
                 within a marginalization
             '''
-            D, Xr = self.D[marginal], X.reshape((X.shape[0],-1))
-            return [np.sum(np.dot(D[:,k], Xr)**2) / total_variance for k in range(D.shape[1])]
+            D, P, Xr = self.D[marginal], self.P[marginal], X.reshape((X.shape[0],-1))
+            return [1 - (np.sum((Xr - np.outer(P[:, k], np.dot(D[:,k], Xr)))**2)) / total_variance for k in range(D.shape[1])]
 
         if marginalization is not None:
             D, Xr         = self.D[marginalization], X.reshape((X.shape[0],-1))